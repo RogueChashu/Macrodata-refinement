a38a73e052f9757264cc1f4fb6d5ba902d80195a
Mélanie Troxell <melanie.dev@proton.me>
2026-01-25 19:14:46 -0500

    Use react-window to implement efficient windowing of large data container and implement keyboard bindings using it

--- mdr-screen/src/Data.jsx
+++ mdr-screen/src/Data.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect, useCallback, useRef } from 'react'
+import { useState, useEffect, useCallback, useRef } from 'react';
+import { FixedSizeGrid as Macrodata } from 'react-window';
 
 // TO DO: write a function to turn some data to 'bad: true'
 // Lumon terminal has 21 columns x 10 rows of visible numbers in some screenshots. 
@@ -8,88 +9,91 @@ function _generateData () {
   const rows = 30;
   const columns = 30;
 
-  const data = Array.from({ length: rows }, () =>  
-    Array.from({ length: columns }, () => ({
+  return Array.from({ length: rows }, (_, rowIndex) =>  
+    Array.from({ length: columns }, (_, dataIndex) => ({
       value: Math.floor(Math.random() * 10),
       delay: Math.random() * 2.5,
       bad: false,
     }))
   );
-  return data
 }
 
 function Data () {
-  const [unrefinedData, setUnrefinedData]  = useState(_generateData);
-  const [marginSize, setMarginSize] = useState({ top: 0, left: 0 });
+  const [scrollPosition, setScrollPosition] = useState({ scrollTop: 0, scrollLeft: 0 });
+  const [gridSize, setGridSize] = useState({ width: 0, height: 0 });
 
+  const unrefinedDataRef = useRef(_generateData());
   const visibleWindowRef = useRef(null);
-  const dataContainerRef = useRef(null);
-  const focusDummyRef = useRef(null);
-  const visibleDataRef = useRef([]);
+  const gridRef = useRef(null);
 
   useEffect(() => {
-    //capture the initial focus when the component mounts, allowing the user to navigate the visibleWindow 
-    // without needing to click on it first: 
-    focusDummyRef.current?.focus()
+    //capture the initial focus when the component mounts, so the user can interact with the data: 
+    visibleWindowRef.current?.focus()
   }, [])
 
   const handleKeyMove = useCallback((e) => {
-    const stepSize = 20
-    const dataRect = dataContainerRef.current?.getBoundingClientRect()
-    const windowRect = visibleWindowRef.current?.getBoundingClientRect()
-    const modify = {}
+    const stepSize = 50;
+    const { scrollTop, scrollLeft } = scrollPosition;
+    let newScrollLeft = scrollLeft;
+    let newScrollTop = scrollTop;
 
-    if (!dataRect || !windowRect) return;
+    if (!gridRef.current) {
+      return;
+    }
+
+    e.preventDefault();
 
     switch (e.key){
       case 'a':
       case 'ArrowLeft':
-        if (dataRect.left + stepSize <= 0) {
-          modify.left = marginSize.left + stepSize
-        }
+        newScrollLeft = Math.max(0, scrollLeft - stepSize);
         break
       case 'd':
       case 'ArrowRight':
-        if (dataRect.right - stepSize >= windowRect.right) {
-          modify.left = marginSize.left - stepSize
-        }
+        newScrollLeft = Math.min(
+          scrollLeft + stepSize,
+          unrefinedDataRef.current[0].length * 75 - gridSize.width
+        );
         break
       case 's':
       case 'ArrowDown':
-        if (dataRect.bottom - stepSize >= windowRect.bottom) {
-          modify.top = marginSize.top - stepSize
-        }
+        newScrollTop = Math.min(
+          scrollTop + stepSize,
+          unrefinedDataRef.current.length * 75 - gridSize.height
+        );
         break
       case 'w':
       case 'ArrowUp':
-        if (dataRect.top + stepSize <= windowRect.top) {
-          modify.top = marginSize.top + stepSize
-        }
+        newScrollTop = Math.max(0, scrollTop - stepSize);
         break
       default:
         break
     }
-    setMarginSize(prevMargin => ({...prevMargin, ...modify }))
-  }, [marginSize])
+    gridRef.current.scrollTo({
+      scrollTop: newScrollTop,
+      scrollLeft: newScrollLeft,
+    })
+    setScrollPosition({ scrollTop: newScrollTop, scrollLeft: newScrollLeft });
+  }, [scrollPosition, gridSize]);
 
+/*
   const handleMouseMove = useCallback((e) => {
-    const cursorX = e.clientX;
-    const cursorY = e.clientY;
     const stepSize = 20
-    const dataRect = dataContainerRef.current?.getBoundingClientRect()
+    const dataRect = dataContainerRef.current?.getBoundingClientRect();
     const windowRect = visibleWindowRef.current?.getBoundingClientRect()
-    const modify = {}
-
+    
     if (!dataRect || !windowRect) return;
 
+    const modify = {}
+
     // 30px detection zone chosen because smaller felt narrow
-    if (cursorX < windowRect.left + 30) { 
+    if (e.clientX < windowRect.left + 30) { 
       if (dataRect.left + stepSize <= 0) { // go to the left
         modify.left = marginSize.left + stepSize;
       } else {
         modify.left = 0;
       }
-    } else if (cursorX > windowRect.right - 30) {
+    } else if (e.clientX > windowRect.right - 30) {
       if (dataRect.right - stepSize >= windowRect.right) { // go the the right
         modify.left = marginSize.left - stepSize;
       } else {
@@ -97,13 +101,13 @@ function Data () {
       }
     }
 
-    if (cursorY < windowRect.top + 30) {
+    if (e.clientY < windowRect.top + 30) {
       if (dataRect.top + stepSize <= windowRect.top) { // go up
         modify.top = marginSize.top + stepSize;
       } else {
         modify.top = 0;
       }
-    } else if (cursorY > windowRect.bottom - 30) {
+    } else if (e.clientY > windowRect.bottom - 30) {
       if (dataRect.bottom - stepSize >= windowRect.bottom) { // go down
         modify.top= marginSize.top - stepSize;
       } else {
@@ -111,34 +115,45 @@ function Data () {
       }
     }
     setMarginSize(prevMargin => ({...prevMargin, ...modify }))
-  }, [marginSize])
+  }, [marginSize]) */
 
   /*
-  const handleMouseOver = (e) => {// useCallback((e) => {
+   useEffect(() => {
+    if (dataContainerRef.current) {
+      dataContainerRef.current.style.setProperty('--margin-left', `${marginSize.left}px`);
+      dataContainerRef.current.style.setProperty('--margin-top', `${marginSize.top}px`);
+    }
+  }, [marginSize])*/
+  /*
+  const handleMouseOver = useCallback((e) => {
     //const radius = 120;
     //const maxScale = 3;
     //const minScale = 1;
     const mouseElement = e.target.closest('.numbers');
-    //console.log(mouseElement);
+    console.log(e.target);
+    console.log(mouseElement);
     //const hoveredIndex = [];
+
+    if (!mouseElement) return
 
     if (mouseElement.id) {
       const [rowIndex, columnIndex] = mouseElement.id.split('-');
-      //console.log('row:', rowIndex, 'col:', columnIndex)
+      console.log('row:', rowIndex, 'col:', columnIndex);
+    }
 
-      for (let i = rowIndex - 1; i = rowIndex + 1; i++) {
-        console.log('i',i)
+    //  for (let i = rowIndex - 1; i = rowIndex + 1; i++) {
+    //    console.log('i',i)
         //for (let j = columnIndex - 1; j = columnIndex +1; j++ ) {
           //console.log('j',j)
           //hoveredIndex.push(i,'-',j)
         //}
         //console.log(hoveredIndex)
-      }
-    }
-  }//,[]); */
+     // }
+    //}
+  },[]);
+  */
 
 /*
-
       //const rect = mouseElement.getBoundingClientRect()
       //console.log(rect)
     } */
@@ -179,89 +194,69 @@ function Data () {
     });
   }, [visibleDataRef.current]); */1
 
-  const isVisibleData = useCallback(() => {
-    const dataContainer = dataContainerRef.current;
-    const visibleWindow = visibleWindowRef.current;
+  function Row({ columnIndex, rowIndex, style }) {
+    const data = unrefinedDataRef.current[rowIndex][columnIndex];
+    return (
+      <div
+        className='numbers'
+        key={`${rowIndex}-${columnIndex}`}
+        id={`${rowIndex}-${columnIndex}`}
+        style={{ 
+          ...style, 
+          '--delay': data.delay 
+        }}
+      >{data.value}</div>
+    );
+  }
 
-    if (dataContainer && visibleWindow) {
-      const visibleChildren = Array.from(dataContainer.children)
-        .map((child) => {
-          const childRect = child.getBoundingClientRect();
-          const windowRect = visibleWindow.getBoundingClientRect();
-          // 50px tolerance zone added in the upcoming bounds check so we don't have non swinging data
-          // on the edges of the visible window.
-          return (
-            childRect.top >= windowRect.top - 50 &&
-            childRect.bottom <= windowRect.bottom + 50 &&
-            childRect.left >= windowRect.left -50 &&
-            childRect.right <= windowRect.right + 50
-          ) 
-            ? child 
-            : null
-        })
-        .filter(child => child !== null)
-      visibleDataRef.current = visibleChildren;
+  useEffect(() => {
+    // This function ensures user can resize window and grid's dimensions will adapt
+    const updateGridSize = () => {
+      if (visibleWindowRef.current) {
+        const { width, height } = visibleWindowRef.current.getBoundingClientRect();
+      setGridSize({ width, height });
+      }
+    }
+
+    updateGridSize();
+  
+    const resizeObserver = new ResizeObserver(updateGridSize)
+    if (visibleWindowRef.current) {
+      resizeObserver.observe(visibleWindowRef.current);
+    }
+
+    return () => {
+      if (visibleWindowRef.current) {
+        resizeObserver.unobserve(visibleWindowRef.current);
+      }
     }
   }, []);
 
-  useEffect(() => {
-    isVisibleData()
-  }, [unrefinedData, marginSize.left, marginSize.top, isVisibleData])
 
   return (
     <div 
-      id='visibleWindow' 
-      ref={visibleWindowRef}
+      id='visibleWindow'
+      ref={visibleWindowRef} 
       onKeyDown={handleKeyMove}
-      onMouseMove={handleMouseMove}
+      //onMouseMove={handleMouseMove}
       //onMouseOver={handleMouseOver}
       //donMouseOut={handleMouseOut}
       tabIndex={-1} 
-      style={{
-        position: 'relative',
-      }}
     >
-      <div 
-        id='dataContainer'
-        ref={dataContainerRef} 
-        style={{
-          position: 'absolute',
-          transform: `translate(${marginSize.left}px, ${marginSize.top}px)`,
-        }}
-      >d
-        {unrefinedData.map((row, rowIndex) => (
-          row.map((data, dataIndex) => {
-            const isCurrentVisibleElem = visibleDataRef.current.some(
-              (element) => element.id === `${rowIndex}-${dataIndex}`
-            );
-          
-            return (
-              <div 
-                className={`numbers ${isCurrentVisibleElem?'swingData' : ''}`}            
-                key={`${rowIndex}-${dataIndex}`}
-                id={`${rowIndex}-${dataIndex}`}
-                style={{
-                  '--delay': data.delay
-                }}
-              >
-                {data.value}
-              </div>
-            )
-          }))
-        )}
-      </div>
-    <div
-      ref={focusDummyRef}
-      tabIndex={0}
-      style={{
-        width: 0,
-        height: 0,
-        opacity: 0,
-        position: 'absolute',
-      }}
-    />
-  </div>
-  )
+      <Macrodata
+        ref={gridRef}
+        className='macrodataContainer'
+        columnCount={30}
+        columnWidth={75}
+        rowCount={unrefinedDataRef.current.length}
+        rowHeight={75}
+        width={gridSize.width}
+        height={gridSize.height}
+      >
+        {Row}
+      </Macrodata>
+    </div>
+  ) 
 }
 
 export default Data
