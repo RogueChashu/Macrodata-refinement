980d812f486615dcdc16f77545364becde0d3f2b
Mélanie Troxell <melanie.dev@proton.me>
2026-01-25 19:13:03 -0500

    Improve overall styling, look and feel

--- mdr-screen/src/Header.jsx
+++ mdr-screen/src/Header.jsx
@@ -1,9 +1,13 @@
+import  lumonLogo  from './assets/lumon1-test.svg'
 
 function Header () {
+  let filename = 'Dranesville'
 
   return (
     <div className='header'>
-      Dranesville
+      <div className='filename'>{filename}</div>
+      <div className='refinementProgress'>19% complete</div>
+      <img className='lumonLogo' src={lumonLogo} />
     </div>
   )
 }
