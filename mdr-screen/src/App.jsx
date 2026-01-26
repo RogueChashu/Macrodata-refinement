b37e7b38fe87697048c79a7c215a3a15993f3f5e
Mélanie Troxell <melanie.dev@proton.me>
2026-01-25 19:13:57 -0500

    Make the visible data section as tall as can be and set a hierarchy for navigation in the data

--- mdr-screen/src/App.jsx
+++ mdr-screen/src/App.jsx
@@ -4,25 +4,18 @@ import Data from './Data'
 import Bins from './Bins'
 import Footer from './Footer'
 
-// Find a similar font
-// Make the logo opaque
-// find out out to make the font "swollen"
-// make the font micro-stripy?
-// change the cursor color to terminal color
-
-
 function App() {
   return (
     <>  
-        <Header />
-        <hr />
-        <hr />
-        <Data />
-        <hr />
-        <hr />
-        <Bins />
-        <hr />
-        <Footer />
+      <Header />
+      <hr />
+      <hr />
+      <Data />
+      <hr />
+      <hr />
+      <Bins />
+      <hr />
+      <Footer />
     </>
   )
 }
--- mdr-screen/src/Data.jsx
+++ mdr-screen/src/Data.jsx
@@ -1,12 +1,12 @@
-// TO DO: write a function to turn some data to bad
+// TO DO: write a function to turn some data to 'bad: true'
 
 
 function _generateData () {
   // Lumon terminal has 21 columns x 10 rows of visible numbers
   // Aiming for a 64 columns x 32 rows numbers matrix -> 2048 numbers.
 
-  const rows = 5 //64
-  const columns = 5 //32
+  const rows = 25 //64
+  const columns = 25 //32
 
   const data = Array.from({length: (rows * columns)}, () => ({
     value: Math.floor(Math.random()*10),
@@ -20,12 +20,14 @@ function Data () {
   const unrefinedData = _generateData()
 
   return (
-    <div className='dataContainer'>
-      {unrefinedData.map((data, index) => {
-        return(
-          <div className='numbers' key={index}>{data.value}</div>
-        )
-      })}
+    <div className='visibleWindow'>
+      <div className='dataContainer'>
+        {unrefinedData.map((data, index) => {
+          return(
+            <div className='numbers' key={index}>{data.value}</div>
+          )
+        })}
+      </div>
     </div>
   )
 }
