469fb887ea3e72b5c8b1922711d4f6893ecf64e4
Mélanie Troxell <melanie.dev@proton.me>
2026-01-25 19:15:01 -0500

    Implement refinement progress in header

--- mdr-screen/src/Header.jsx
+++ mdr-screen/src/Header.jsx
@@ -1,14 +1,22 @@
-import  lumonLogo  from './assets/lumon1-test.svg'
+import  lumonLogo  from './assets/lumon1-test.svg';
+import { useEffect } from 'react';
 
 //TO DO: make the background opaque behind the Lumon globe logo
 
-function Header () {
+function Header ({ refinementProgress }) {
   const filename = 'Siena'
+
+  //console.log('in header:', refinementProgress.totalBadData)
+ // useEffect(() )
 
   return (
     <div className='header'>
       <div className='filename'>{filename}</div>
-      <div className='refinementProgress'>0% complete</div>
+      <div className='refinementProgress'>
+        {refinementProgress.refined === 0 ? 
+          `0`: 
+          Math.round(refinementProgress.refined / refinementProgress.totalBadData * 100)}% complete
+      </div>
       <img className='lumonLogo' src={lumonLogo} />
     </div>
   )
