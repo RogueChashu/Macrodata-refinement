9e1a37ff728ba17b209a14362962fd45d5531e0f
Mélanie Troxell <melanie.dev@proton.me>
2026-03-02 21:42:06 -0500

    feature: Add scan lines to emulate Data General Dasher monitor

--- mdr-screen/src/App.jsx
+++ mdr-screen/src/App.jsx
@@ -127,6 +127,7 @@ function App() {
         </div>
         <hr />
         <Footer />
+        <div className='CRT-scanlines-overlay' />
         <div className='CRT-rollingBar-overlay' />
       </div>
     </>
